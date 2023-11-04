8df60b9ff38abfc2aa34d070aceee7f3bf31d0cb
Contentoni <tonimohe@hotmail.com>
2023-11-04 00:29:26 -0300

    pusheo variables para poder testear en otra compuu

---
+++ tp2/variables.py
@@ -0,0 +1,26 @@
+#------------------------------------------
+
+webcamId = 1
+
+#------------------------------------------
+
+squareBase = r"C:\Users\tonim\OneDrive - URV\UNIVERSIDAD\ERASMUS 2023-2024\ASIGNATURAS\Vision_Artificial\testing\cuadradobase.png"
+circleBase = r"C:\Users\tonim\OneDrive - URV\UNIVERSIDAD\ERASMUS 2023-2024\ASIGNATURAS\Vision_Artificial\testing\circulobase.png"
+triangleBase = r"C:\Users\tonim\OneDrive - URV\UNIVERSIDAD\ERASMUS 2023-2024\ASIGNATURAS\Vision_Artificial\testing\triangulobase.png"
+
+#------------------------------------------
+
+square1 = r"C:\Users\tonim\OneDrive - URV\UNIVERSIDAD\ERASMUS 2023-2024\ASIGNATURAS\Vision_Artificial\testing\cuadrado-1.png"
+
+#------------------------------------------
+
+circle1 = r"C:\Users\tonim\OneDrive - URV\UNIVERSIDAD\ERASMUS 2023-2024\ASIGNATURAS\Vision_Artificial\testing\circulo-1.png"
+
+#------------------------------------------
+
+triangle1= r"C:\Users\tonim\OneDrive - URV\UNIVERSIDAD\ERASMUS 2023-2024\ASIGNATURAS\Vision_Artificial\testing\triangulo-1.png"
+triangle2= r"C:\Users\tonim\OneDrive - URV\UNIVERSIDAD\ERASMUS 2023-2024\ASIGNATURAS\Vision_Artificial\testing\triangulo-2.png"
+triangle3= r"C:\Users\tonim\OneDrive - URV\UNIVERSIDAD\ERASMUS 2023-2024\ASIGNATURAS\Vision_Artificial\testing\triangulo-3.png"
+triangle4= r"C:\Users\tonim\OneDrive - URV\UNIVERSIDAD\ERASMUS 2023-2024\ASIGNATURAS\Vision_Artificial\testing\triangulo-4.png"
+triangle5= r"C:\Users\tonim\OneDrive - URV\UNIVERSIDAD\ERASMUS 2023-2024\ASIGNATURAS\Vision_Artificial\testing\triangulo-5.png"
+triangle6= r"C:\Users\tonim\OneDrive - URV\UNIVERSIDAD\ERASMUS 2023-2024\ASIGNATURAS\Vision_Artificial\testing\triangulo-6.png"
